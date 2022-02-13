3c34211d231e4c08998e80d9d78c64bed583bef4
zjd980501 <jzhang482@fordham.edu>
2022-02-13 00:07:28 -0500

    second commit

---
+++ pythonJindi.py
@@ -0,0 +1,5 @@
+# Jindi Zhang Assignment 1A
+s=float(input('How far will you run(in miles)? '))
+a=float(input('How fast will you run(in minutes/mile; enter decimal,like7.5for7:30pace)? '))
+t=s*a
+print('It will take you',t,'minutes to run',s,'miles')
